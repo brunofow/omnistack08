e119ce3fe6d57cbd74e548aba96c2e98abe72661
brunofow <bruocamposfow@gmail.com>
2019-08-10 01:23:21 -0300

    Mobile Pronto

---
+++ tindev/src/index.js
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+
+import Routes from './routes';
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+
+
+export default App;
+
